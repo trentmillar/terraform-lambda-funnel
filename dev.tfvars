460784b79d5de517e549a06ab0a8253ac7fd523f
trentmillar <trent.millar@gmail.com>
2020-02-05 21:15:32 -0700

    add variables for SNS, emails

--- dev.tfvars
+++ dev.tfvars
@@ -1,1 +1,2 @@
 region          = "us-west-2"
+email_addresses = []
--- variables.tf
+++ variables.tf
@@ -1,3 +1,14 @@
 variable "region" {
   type = string
 }
+
+variable "email_addresses" {
+  type        = "list"
+  description = "Email address to send notifications to"
+}
+
+variable "protocol" {
+  default     = "email"
+  description = "SNS Protocol to use. email or email-json"
+  type        = "string"
+}
