7fd5e8a85087fc6b8d0eeb403832f3b6321f0b92
trentmillar <trent.millar@gmail.com>
2020-02-05 21:11:43 -0700

    fn looks up instance details but fails on SNS lookup - add role to fn

--- lambda/echo.py
+++ lambda/echo.py
@@ -2,14 +2,27 @@
 
 import os
 import json
+import datetime
 
 import boto3
 
 
-ec2_client = boto3.client("ec2")
+EC2_CLIENT = boto3.client("ec2")
+SNS_CLIENT = boto3.client("sns")
+
+
+def datetime_converter(o):
+    if isinstance(o, datetime.datetime):
+        return o.isoformat()
 
 
 def lambda_handler(event, context):
-
-    print("sns -> " + os.environ.get("sns_arn")
+    print("sns -> " + os.environ.get("sns_arn"))
     print("event -> " + json.dumps(event, indent=2))
+
+    sns_subscription_arn = os.environ.get("sns_arn")
+    instance_id = event["detail"]["instance-id"]
+
+    print(json.dumps(EC2_CLIENT.describe_instances(InstanceIds = [instance_id]), indent=2, default=datetime_converter))
+
+    print(json.dumps(SNS_CLIENT.list_topics(), indent=2))
