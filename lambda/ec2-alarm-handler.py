d70616b1d3f6367e61e73e289f6ce6329acbca33
trentmillar <trent.millar@gmail.com>
2020-02-07 10:58:23 -0700

    lambda looks up metrics and creates info struct

--- lambda/ec2-alarm-handler.py
+++ lambda/ec2-alarm-handler.py
@@ -9,11 +9,24 @@ import boto3
 
 EC2_CLIENT = boto3.client("ec2")
 SNS_CLIENT = boto3.client("sns")
+CW_CLIENT = boto3.client('cloudwatch')
 
 
 def datetime_converter(o):
-    if isinstance(o, datetime.datetime):
+    if isinstance(o, datetime):
         return o.isoformat()
+        
+        
+def parse_prefix(line, fmt):
+    try:
+        t = datetime.datetime.strptime(line, fmt)
+    except ValueError as v:
+        if len(v.args) > 0 and v.args[0].startswith('unconverted data remains: '):
+            line = line[:-(len(v.args[0]) - 26)]
+            t = datetime.datetime.strptime(line, fmt)
+        else:
+            raise
+    return t        
 
 
 def get_ec2_status(instance_id):
@@ -21,9 +34,8 @@ def get_ec2_status(instance_id):
 
 
 def get_message_details(message):
-    print(message)
     detail = {
-        "instances": []
+        "ec2_instances": []
     }
     detail["state"] = message["NewStateValue"]
     detail["description"] = message["AlarmDescription"]
@@ -32,14 +44,47 @@ def get_message_details(message):
     detail["comparison"] = message["Trigger"]["ComparisonOperator"]
     
     for metric in message["Trigger"]["Metrics"]:
-        print(metric)
         if "Expression" not in metric:
             detail["metric_name"] = metric["MetricStat"]["Metric"]["MetricName"]
             detail["namespace"] = metric["MetricStat"]["Metric"]["Namespace"]
-            detail["instances"].append(metric["MetricStat"]["Metric"]["Dimensions"][0]["value"])
-        
-    
-    print(detail)
+            
+            end = parse_prefix(detail["datetime"], "%Y-%m-%dT%H:%M:%S.%f")
+            
+            instance_details = {
+                "ec2_instance_id": metric["MetricStat"]["Metric"]["Dimensions"][0]["value"],
+                "metric_end": end,
+                "metric_start": end - datetime.timedelta(minutes=10)
+            }
+            
+            # fetch metrics
+            response = CW_CLIENT.get_metric_data(
+                MetricDataQueries=[
+                    {
+                        'Id': metric["Id"],
+                        'MetricStat': {
+                            'Metric': {
+                                'Namespace': metric["MetricStat"]["Metric"]["Namespace"],
+                                'MetricName': metric["MetricStat"]["Metric"]["MetricName"],
+                                'Dimensions': [
+                                    {
+                                        "Name": metric["MetricStat"]["Metric"]["Dimensions"][0]["name"],
+                                        "Value": metric["MetricStat"]["Metric"]["Dimensions"][0]["value"]
+                                    }
+                                ]
+                            },
+                            'Period': metric["MetricStat"]["Period"],
+                            'Stat': metric["MetricStat"]["Stat"],
+                        },
+                        'Label': "CPU Utilization",
+                    },
+                ],
+                StartTime=instance_details["metric_start"],
+                EndTime=instance_details["metric_end"]
+            )
+            
+            instance_details["metric_data"] = response["MetricDataResults"][0]["Values"]
+            detail["ec2_instances"].append(instance_details)
+            
     return detail
 
 
@@ -70,9 +115,6 @@ def get_message_details(message):
 
 
 def lambda_handler(event, context):
-    #print("sns -> " + os.environ.get("sns_arn"))
-    #print("event -> " + json.dumps(event, indent=2))
-
     sns_records = event["Records"]
 
     if len(sns_records) == 0:
@@ -81,13 +123,19 @@ def lambda_handler(event, context):
     # Assume single record
     cw_message = json.loads(sns_records[0]["Sns"]["Message"])
     cw_details = get_message_details(cw_message)
-    
-    ec2_details = EC2_CLIENT.describe_instances(InstanceIds = cw_details["instances"] )
-    print(json.dumps(ec2_details, indent=2, default=datetime_converter))
 
+    instance_ids = []
+    for instance in cw_details["ec2_instances"]:
+        instance_ids.append(instance["ec2_instance_id"])
+    
+    #ec2_details = EC2_CLIENT.describe_instances(InstanceIds = instance_ids )
+    #json.dumps(cw_details, default=datetime_converter)
+    
+    print(cw_details)
+    
     SNS_CLIENT.publish(
         TopicArn=os.environ.get("sns_arn"),
         Subject="Alarm",
-        Message=json.dumps({"default": json.dumps(ec2_details, indent=2, default=datetime_converter)}),
+        Message=json.dumps({"default": None}),
         MessageStructure="json",
     )
